29f8d5841d28d667e896ac5b3b45bf5ad1d1f7b3
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-18 08:36:55 -0500

    "finished looping array"

--- src/javaArrays/Runner.java
+++ src/javaArrays/Runner.java
@@ -3,7 +3,12 @@ package javaArrays;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] myArr = {10,20,30,40,50,60,70,80,90,100};
+		
+		for (int i = 0; i<myArr.length ;i++) {
+			System.out.println("This is the index " + i + ", this the what is in the array " + myArr[i]);
+			
+		};
 
 	}
 
